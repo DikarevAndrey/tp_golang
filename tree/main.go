65ae4e78d94f7c4c82f57661349d08a9701e2a48
DikarevAndrey <dias7707@gmail.com>
2018-10-09 00:02:50 +0300

    Completed tree utility.

--- tree/main.go
+++ tree/main.go
@@ -1,13 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+	"strconv"
 )
 
-func dirTree(outStream io.Writer, path string, printFiles bool) error {
+func isLastDir(dir os.FileInfo, path string) bool {
+	dirEntries, _ := ioutil.ReadDir(path)
+	dirs := []os.FileInfo{}
+	for _, entry := range dirEntries {
+		if entry.IsDir() {
+			dirs = append(dirs, entry)
+		}
+	}
+	return dir.Name() == dirs[len(dirs)-1].Name()
+}
+
+func recDirTree(outputStream io.Writer, path string, printFiles bool, indent string) (resultErr error) {
+	dirEntries, err := ioutil.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	for i, entry := range dirEntries {
+		if entry.IsDir() {
+			dirIndent := indent + "├───%s\n"
+			levelIndent := "│"
+			if (!printFiles && isLastDir(entry, path)) || (printFiles && i == len(dirEntries)-1) {
+				dirIndent = indent + "└───%s\n"
+				levelIndent = ""
+			}
+			fmt.Fprintf(outputStream, dirIndent, entry.Name())
+
+			err := recDirTree(outputStream, path+"/"+entry.Name(), printFiles, indent+levelIndent+"\t")
+			if err != nil {
+				return err
+			}
+		} else if printFiles {
+			fileIndent := indent + "├───%s (%s)\n"
+			if i == len(dirEntries)-1 {
+				fileIndent = indent + "└───%s (%s)\n"
+			}
+			size := strconv.FormatInt(entry.Size(), 10) + "b"
+			if size == "0b" {
+				size = "empty"
+			}
+			fmt.Fprintf(outputStream, fileIndent, entry.Name(), size)
+		}
+	}
 
 	return nil
+}
+
+func dirTree(outputStream io.Writer, path string, printFiles bool) (resultErr error) {
+	return recDirTree(outputStream, path, printFiles, "")
 }
 
 func main() {
--- tree/tree.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"io"
-)
-
-func dirTree(outStream io.Writer, path string, printFiles bool) string {
-	return "Test\n"
-}
